76662d952bf2b2215ef15b0943934cd6cb0d40c7
Qurat <dragon.ace2020@gmail.com>
2020-04-21 03:33:18 +0400

    Make smooth scrolling to make a mask section slower

--- js/main.js
+++ js/main.js
@@ -32,6 +32,43 @@ function openTab(id1, id2) {
     document.getElementById(id2).className  += " active";
 }
 
+// Element to move, time in ms to animate
+function scrollTo(element, duration) {
+    var e = document.documentElement;
+    if(e.scrollTop===0){
+        var t = e.scrollTop;
+        ++e.scrollTop;
+        e = t+1===e.scrollTop--?e:document.body;
+    }
+    scrollToC(e, e.scrollTop, element, duration);
+}
+
+// Element to move, element or px from, element or px to, time in ms to animate
+function scrollToC(element, from, to, duration) {
+    if (duration <= 0) return;
+    if(typeof from === "object")from=from.offsetTop;
+    if(typeof to === "object")to=to.offsetTop;
+
+    scrollToX(element, from, to, 0, 1/duration, 20, easeOutCuaic);
+}
+
+function scrollToX(element, xFrom, xTo, t01, speed, step, motion) {
+    if (t01 < 0 || t01 > 1 || speed<= 0) {
+        element.scrollTop = xTo;
+        return;
+    }
+    element.scrollTop = xFrom - (xFrom - xTo) * motion(t01);
+    t01 += speed * step;
+
+    setTimeout(function() {
+        scrollToX(element, xFrom, xTo, t01, speed, step, motion);
+    }, step);
+}
+function easeOutCuaic(t){
+    t--;
+    return t*t*t+1;
+}
+
 var makeAMaskArray = document.querySelectorAll("a[href='#make-a-mask']");
 
 Array.from(makeAMaskArray).forEach(function(element) {
@@ -42,11 +79,12 @@ function makeAMaskEvent (e) {
   e.preventDefault();
   e.stopPropagation();
   var element = document.getElementById('make-a-mask').offsetTop;
-  window.scrollTo({
-    top: element - 50,
-    behavior: "smooth",
-    block: 'start'
-  });
+  // window.scrollTo({
+  //   top: element - 50,
+  //   behavior: "smooth",
+  //   block: 'start'
+  // });
+  scrollTo(element - 50, 4000);
   removeNavActiveClass();
 }
 
