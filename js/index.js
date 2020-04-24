6ea7961a95575fc6f82929ebaa38ccd73ba2e393
eohomegrownapps <euan.l.y.ong@gmail.com>
2020-04-24 15:36:57 +0100

    Added auto-update for province data; added date and sources for estimated infections and confirmed data
    As in title

--- js/index.js
+++ js/index.js
@@ -1,41 +1,38 @@
-axios.get('https://covidapi.info/api/v1/country/PAK/latest')
-    .then(function (res) {
-        var result = res && res.data && res.data.result;
-        var date = result && Object.keys(result)[0];     // result has only 1 key, the date of the last update
-        var cases = result && result[date];
-
-        if (date) {
-            var dateParts = date.split('-');
-            if (dateParts.length === 3) {
-                document.getElementById('estimated-infections-date').innerText = `${dateParts[1]}/${dateParts[2]}/${dateParts[0]}`;
-            }
-        }
-
-        if (cases.confirmed) {
-            document.getElementById('confirmed-cases-num').innerText = cases.confirmed;
-        }
-
-        if (cases.deaths) {
-            document.getElementById('confirmed-deaths-num').innerText = cases.deaths;
-        }
-
-        if (cases.recovered) {
-            document.getElementById('confirmed-recovered-num').innerText = cases.recovered;
-        }
-    })
-    .catch(function () {
-        console.error('Error fetching data from covidapi.info');
-    });
-
 axios.get('https://storage.googleapis.com/static-covid/static/data-main-v4.json')
     .then(function (res) {
         var pakistanData = res && res.data && res.data.regions && res.data.regions.PK;
         var estimatedCases = pakistanData && pakistanData.CurrentEstimate;
-
         if (estimatedCases) {
-            document.getElementById('estimated-infections-num').innerText = estimatedCases;
+            document.getElementById('estimated-infections-num').innerText = estimatedCases.toLocaleString('en-pk');
+            if (res.data.created){
+                document.getElementById('estimated-infections-date').innerText = new Date(res.data.created).toLocaleString('en-PK',{year: 'numeric', month: '2-digit', day: '2-digit'});
+            }
         }
     })
     .catch(function () {
         console.error('Error fetching data from epidemicforecasting.org');
+    });
+
+axios.get('files/gov_data.json')
+    .then(function (res) {
+        var pakistanData = res && res.data;
+
+        for (var t of ["cases", "deaths", "recovered"]){
+            if (pakistanData[t]["total"]){
+                document.getElementById('confirmed-'+t+'-num').innerText = pakistanData[t]["total"].toLocaleString('en-pk');
+            }
+        }
+
+        for (var p of ["sindh", "punjab", "kp", "balochistan"]){
+            if (pakistanData["cases"][p]){
+                document.getElementById('confirmed-'+p).innerText = pakistanData["cases"][p].toLocaleString('en-pk');
+            }
+        }
+
+        if (res.data.lastupdated){
+            document.getElementById('confirmed-infections-date').innerText = new Date(res.data.lastupdated).toLocaleString('en-PK',{year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute:'2-digit'});
+        }
+    })
+    .catch(function () {
+        console.error('Error fetching province data');
     });
