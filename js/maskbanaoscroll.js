c0e8f19f61c09706ded8a7bedacb0c7736ba8883
Kyra del Rosario <kyra@byimplication.com>
2020-04-25 23:35:21 +0800

    Mask page and make a mask menu

---
+++ js/maskbanaoscroll.js
@@ -0,0 +1,26 @@
+var height = $( window ).height();
+var halfHeight = height/2;
+
+$( window ).resize(function() {
+    height = $(window).height();
+    halfHeight = height/2;
+  });
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    var pos = Math.floor(scroll/halfHeight);
+    var newPos = pos+1;
+
+    console.log("new Post " + newPos)
+    if ((pos != 0) && (pos != 10)) {
+        console.log(newPos);
+        $('.image-container img').attr("src","images/mask/"+ newPos +".png");
+    }
+    else if (pos === 10) {
+        $('.image-container img').attr("src","images/mask/10.png");
+    }
+    else {
+        $('.image-container img').attr("src","images/mask/1.png");
+    }
+    console.log("old pos" + pos);
+});
