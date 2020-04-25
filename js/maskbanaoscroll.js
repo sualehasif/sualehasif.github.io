9b12d987873cc2c9db95e19585fd2347a3309017
Kyra del Rosario <kyra@byimplication.com>
2020-04-26 01:18:06 +0800

    Responsiveness for mask tutorial

--- js/maskbanaoscroll.js
+++ js/maskbanaoscroll.js
@@ -1,9 +1,21 @@
+width = $(window).width();
 var height = $( window ).height();
 var halfHeight = height/2;
+
+if (width <= 768) {
+    height = ($( window ).height()/2);
+    halfHeight = height/2;
+}
+
 
 $( window ).resize(function() {
     height = $(window).height();
     halfHeight = height/2;
+    width = $(window).width();
+    if (width <= 768) {
+        height = ($( window ).height()/2);
+        halfHeight = height/2;
+    }
   });
 
 $(window).scroll(function (event) {
@@ -16,7 +28,7 @@ $(window).scroll(function (event) {
         console.log(newPos);
         $('.image-container img').attr("src","images/mask/"+ newPos +".png");
     }
-    else if (pos === 10) {
+    else if (pos >= 10) {
         $('.image-container img').attr("src","images/mask/10.png");
     }
     else {
