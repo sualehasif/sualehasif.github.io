ef2447813fd37c55f934f6b75e3240af16243f66
Mateo Simonovic <mateosimonovic@gmail.com>
2020-04-28 03:53:28 +0200

    Add snappy scroll

--- js/index.js
+++ js/index.js
@@ -28,13 +28,18 @@ new Glide('.glide', {
 
 // Handle frame animations on scroll
 var counterAnimationStarted = false;
+var counterAnimationCompleted = false;
 var activeFrameIndex = 0;
+var scrollY = 0;
+var scrollActive = false;
+var numFrames = $('.frame').length;
 
 function counterDone() {
     $('#counter').addClass('c-danger');
     setTimeout(function () {
         $('#counter-seconds').fadeIn(1000).css("display", "inline-block");
         setTimeout(function () {
+            counterAnimationCompleted = true;
 
             // TODO - change this from a fade to a slide to right
             $('.counter-additional').addClass('visible');
@@ -81,5 +86,43 @@ function showVisibleFrame() {
     }
 }
 
-$(window).scroll(showVisibleFrame);
+function scrollToNextSection() {
+    if (scrollActive || (counterAnimationStarted && !counterAnimationCompleted)) {
+        return false;
+    }
+
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+    activeFrameIndex = (st > scrollY) ? activeFrameIndex + 1 : activeFrameIndex - 1;
+    if (activeFrameIndex < 0) {
+        activeFrameIndex = 0;
+    } else if (activeFrameIndex >= numFrames) {
+        activeFrameIndex = numFrames - 1;
+    }
+
+    scrollActive = true;
+    $(window).scrollTo($(`.frame:eq(${activeFrameIndex})`), 1000, function() {
+        setTimeout(function() {
+            scrollActive = false;
+            var st = window.pageYOffset || document.documentElement.scrollTop;
+            scrollY = st <= 0 ? 0 : st;
+        }, 100);
+
+        $(`.frame-content:eq(${activeFrameIndex-1})`).removeClass('visible');
+
+        $(`.frame-content:eq(${activeFrameIndex})`).addClass('visible');
+        if (activeFrameIndex === 0) {
+            $('#arrows').addClass('visible');
+            return;
+        } else {
+            $('#arrows').removeClass('visible');
+        }
+
+        if (activeFrameIndex === 5 && !counterAnimationStarted) {
+            counterAnimationStarted = true;
+            animateCounter('counter', 0, 40, 1500, counterDone);
+        }
+    });
+}
+
+$(window).scroll(scrollToNextSection);
 $(document).ready(showVisibleFrame);
